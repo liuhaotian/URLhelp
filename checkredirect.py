cde45538636fdd91842768486ec63f1f08fa1a54
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-05 00:53:26 -0600

    Update usage info

--- checkredirect.py
+++ checkredirect.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
+#
+#   Author: Haotian Liu
+#   Email:  liuhaotian.air@gmail.com
+#
+#   usage:  from checkredirect import checkRedirect
+#           checkRedirect('your url')
+#
+
 import httplib
 from urlparse import urlparse
 
 def checkRedirect(url):
-	try:
-		urlObj = urlparse(url)
-		conn = httplib.HTTPConnection(urlObj.netloc)
-		conn.request("HEAD", urlObj.path)
-		res = conn.getresponse()
-		return [x[1] for x in res.getheaders() if x[0] =='location'][0]
-	except Exception, e:
-		return url
+    try:
+        urlObj = urlparse(url)
+        conn = httplib.HTTPConnection(urlObj.netloc)
+        conn.request("HEAD", urlObj.path)
+        res = conn.getresponse()
+        return [x[1] for x in res.getheaders() if x[0] =='location'][0]
+    except Exception, e:
+        return url
 
 def main():
-	print checkRedirect('http://bit.ly/Yj18wU')
+    print checkRedirect('http://bit.ly/Yj18wU')
 
 if __name__ == '__main__':
-	main()
+    main()
