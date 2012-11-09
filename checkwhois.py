3947c794143fa57f83d1ecde8449f4b514dadbd3
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-09 14:35:00 -0600

    Able to get the datetime

--- checkwhois.py
+++ checkwhois.py
@@ -8,12 +8,34 @@
 #
 
 import subprocess
+import re
 from urlparse import urlparse
+from dateutil import parser
 
 def checkWhois(url):
     try:
-        urlObj = urlparse(url)
-        return subprocess.check_output(['whois',urlObj.netloc])
+        urlObj  = urlparse(url)
+        retdata = subprocess.check_output(['whois',urlObj.netloc])
+        try:
+            expire_date = re.search(r'[Ee][Xx][Pp][Ii][Rr][^:]*:(.*)',retdata).groups()[0]
+            expire_date = parser.parse(expire_date)
+        except Exception, e:
+            expire_date = parser.parse('1970-01-01 00:00:00')
+
+        try:
+            update_date = (re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]*[Dd][Aa][Tt][Ee][^:]*:(.*)',retdata) or re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]*:(.*)',retdata)).groups()[0]
+            update_date = parser.parse(update_date)
+        except Exception, e:
+            update_date = parser.parse('1970-01-01 00:00:00')
+
+        try:
+            create_date = (re.search(r'[Rr][Ee][Gg][Ii][Ss][Tt][^:]*[Dd][Aa][Tt][Ee][^:]*:(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][Ee][^:]*[Dd][Aa][Tt][Ee][^:]*:(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][Ee][^:]*:(.*)',retdata)).groups()[0]
+            create_date = parser.parse(create_date)
+        except Exception, e:
+            create_date = parser.parse('1970-01-01 00:00:00')
+        
+
+        return create_date, update_date, expire_date
     except Exception, e:
         return url
 
