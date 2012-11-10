5c2972b124c7218b5707787c527e3ea2d72b35f1
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-10 17:04:14 -0600

    More choice

--- whois.py
+++ whois.py
@@ -7,17 +7,56 @@
 #           whois('your domain')
 #
 import socket
+import re
 
-def whois(domain):
-    HOST = 'whois.verisign-grs.com'
+def whois(domain, host = None):
+    if host is None:
+        HOST = domain.rsplit('.',1)[1] + '.whois-servers.net'
+    else:
+        HOST = 'whois.verisign-grs.com'
     PORT = 43 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    s.sendall('=' + domain + '\r\n')
-    data = s.recv(65536) + s.recv(65536)
+    s.sendall(domain + '\r\n')
+
+    data = s.recv(65536)
+    length = 0
+    while length < len(data):
+        length = len(data)
+        data += s.recv(65536)
     s.close()
-    return data
+
+    return findhost(data) is not None and whois(domain, findhost(data)) or "\"=xxx\"" in data and whois('=' + domain) or data
+
+def findhost(rawdata):
+    whoisserv = [\
+    "whois.abuse.net",\
+    "whois.crsnic.net",\
+    "whois.networksolutions.com",\
+    "whois.nic.mil",\
+    "whois.nic.gov",\
+    "whois.arin.net",\
+    "whois.lacnic.net",\
+    "whois.ripe.net",\
+    "whois.apnic.net",\
+    "whois.ra.net",\
+    "whois.6bone.net",\
+    "whois.registro.br",\
+    "whois.norid.no",\
+    "whois.iana.org"]
+    try:
+        server = re.search(r'Whois Server:[^\w]*([\.\w]*)',rawdata).groups()[0]
+    except Exception, e:
+        return None
+    
+    if server in whoisserv:
+        return server
+    elif 'whois-servers' in server.rsplit('.'):
+        return server
+    else:
+        return None
+
 
 def main():
     print whois('t.co')
