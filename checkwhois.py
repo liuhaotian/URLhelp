4a71549eacb0fe18b6fab159628a6480048b0e15
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-10 17:30:25 -0600

    More precise

--- checkwhois.py
+++ checkwhois.py
@@ -19,19 +19,19 @@ def checkWhois(url):
         retdata = whois(urlObj.netloc)
         #retdata = subprocess.check_output(['whois',urlObj.netloc])
         try:
-            expire_date = re.search(r'[Ee][Xx][Pp][Ii][Rr][^:]{0,20}:(.*)',retdata).groups()[0]
+            expire_date = re.search(r'[Ee][Xx][Pp][Ii][Rr][^:]{0,20}[:OoNn]{1,2}(.*)',retdata).groups()[0]
             expire_date = parser.parse(expire_date)
         except Exception, e:
             expire_date = parser.parse('1970-01-01 00:00:00')
 
         try:
-            update_date = (re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}:(.*)',retdata) or re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]{0,20}:(.*)',retdata)).groups()[0]
+            update_date = (re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}[:OoNn]{1,2}(.*)',retdata) or re.search(r'[Uu][Pp][Dd][Aa][Tt][Ee][^:]{0,20}[:OoNn]{1,2}(.*)',retdata)).groups()[0]
             update_date = parser.parse(update_date)
         except Exception, e:
             update_date = parser.parse('1970-01-01 00:00:00')
 
         try:
-            create_date = (re.search(r'[Rr][Ee][Gg][Ii][Ss][Tt][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}:(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][EeIi][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}:(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][Ee][^:]{0,20}:(.*)',retdata)).groups()[0]
+            create_date = (re.search(r'[Rr][Ee][Gg][Ii][Ss][Tt][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}[:OoNn]{1,2}(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][EeIi][^:]{0,20}[Dd][Aa][Tt][Ee][^:]{0,20}[:OoNn]{1,2}(.*)',retdata) or re.search(r'[Cc][Rr][Ee][Aa][Tt][Ee][^:]{0,20}[:OoNn]{1,2}(.*)',retdata)).groups()[0]
             create_date = parser.parse(create_date)
         except Exception, e:
             create_date = parser.parse('1970-01-01 00:00:00')
