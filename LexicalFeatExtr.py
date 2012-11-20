cd4d63094a93a0ed72d4b7f4269bc84c5ec10ceb
Zhengyang Qu <ZhengyangQu2017@u.northwestern.edu>
2012-11-19 20:10:33 -0600

    Add new features extraction
    Domain token count, path token count, average domain token length,
    average path token length, longest domain token length, longest path
    token length

--- LexicalFeatExtr.py
+++ LexicalFeatExtr.py
@@ -28,20 +28,58 @@ class LexicalFeature:
         if urlline.find('/')>0:
           hostportion=urlline[:urlline.find('/')]
           pathportion=urlline[urlline.find('/')+1:]
-        #print hostportion
-        #if pathportion[len(pathportion)-1]=='\n' or  pathportion[len(pathportion)-1]=='\r':
-          #pathportion=pathportion[:len(pathportion)-1]
-        lenhost=len(hostportion) # the length of host portion
-        lenurl=len(urlline) # the length of urllink
-        numofdots=self.GetNumofSpecChar('.',urlline)# the number of dots in url
-        TLD=self.GetTLD(hostportion) #get top-level domain
+       
+        # the length of host portion
+        lenhost=len(hostportion) 
+        
+        # the length of urllink
+        lenurl=len(urlline)
+        
+        # the number of dots in url
+        numofdots=self.GetNumofSpecChar('.',urlline)
+        
+        #get top-level domain
+        TLD=self.GetTLD(hostportion) 
+        
         bagofwordsinhost=[]
         bagofwordsinpath=[]
         tempstring=hostportion
+        
         #get the bag-of-words in host portion
         bagofwordsinhost=self.GetBagofWords(hostportion)
+        
         #get the bag-of-words in path portion
         bagofwordsinpath=self.GetBagofWords(pathportion)
+        
+        #get the count of domain token
+        domaintokencount=self.GetTokenCount(self.GetDomainToken(bagofwordsinhost))
+        domaintokencount=domaintokencount+self.GetTokenCount(self.GetDomainToken(bagofwordsinpath))
+         
+        #get the path token count
+        pathtokencount=self.GetTokenCount(bagofwordsinpath)
+        
+        #get the average domain token length
+        if domaintokencount==0:
+          avrdomaintokenlength=0
+        if domaintokencount>0:
+          avrdomaintokenlength=(self.GetTotalTokenLength(self.GetDomainToken(bagofwordsinhost))+self.GetTotalTokenLength(self.GetDomainToken(bagofwordsinpath)))/(self.GetTokenCount(self.GetDomainToken(bagofwordsinhost))+self.GetTokenCount(self.GetDomainToken(bagofwordsinpath)))
+        
+        #get the average path token length
+        if pathtokencount==0:
+          avrpathtokenlength=0
+        if pathtokencount>0:
+          avrpathtokenlength=self.GetTotalTokenLength(bagofwordsinpath)/self.GetTokenCount(bagofwordsinpath)
+        
+        #get the longest domain token length
+        if self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinhost))>=self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinpath)):
+          longestdomaintokenlength=self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinhost))
+        if self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinhost))<self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinpath)):
+          longestdomaintokenlength=self.GetLongestTokenLength(self.GetDomainToken(bagofwordsinpath))
+          
+        #get the longest path token length
+        longestpathtokenlength=self.GetLongestTokenLength(bagofwordsinpath)
+        
+        
         outstr = "%s"%lenhost
         file_output.write(outstr)
         file_output.write(' ')
@@ -53,17 +91,35 @@ class LexicalFeature:
         file_output.write(' ')
         file_output.write(TLD)
         file_output.write(' ')
-        #print bagofwordsinhost
-        #print bagofwordsinpath
-        for elem in bagofwordsinhost:
-          if elem != '':
-            file_output.write(elem)
-            file_output.write(' ')
-        for elem in bagofwordsinpath:
-          if elem != '':
-            file_output.write(elem)
+        outstr = "%s"%domaintokencount
+        file_output.write(outstr)
+        file_output.write(' ')
+        outstr = "%s"%pathtokencount
+        file_output.write(outstr)
+        file_output.write(' ')
+        outstr = "%s"%avrdomaintokenlength
+        file_output.write(outstr)
+        file_output.write(' ')
+        outstr = "%s"%avrpathtokenlength
+        file_output.write(outstr)
+        file_output.write(' ')
+        outstr = "%s"%longestdomaintokenlength
+        file_output.write(outstr)
+        file_output.write(' ')
+        outstr = "%s"%longestpathtokenlength
+        file_output.write(outstr)
+        file_output.write(' ')
+      
+        #for elem in bagofwordsinhost:
+          #if elem != '':
+            #file_output.write(elem)
+            #file_output.write(' ')
+        #for elem in bagofwordsinpath:
+          #if elem != '':
+            #file_output.write(elem)
             #if elem != bagofwordsinpath[len(bagofwordsinpath)-1]: 
-            file_output.write(' ')
+            #file_output.write(' ')
+       
         file_output.write('\n')
 
 
@@ -97,7 +153,38 @@ class LexicalFeature:
         portion=portion[portion.find(char)+1:]
     bagofwords.append(portion)
     return bagofwords
+    
 
+  def GetDomainToken(self, bagofwords):
+    domainset=['ac', 'ad', 'ae', 'aero', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao', 'aq', 'ar', 'arpa', 'as', 'asia', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'biz', 'bj', 'bm', 'bn', 'bo', 'br', 'bs', 'bt', 'bv', 'bw', 'by', 'bz', 'ca', 'cat', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'com', 'coop', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'edu', 'ee', 'eg', 'er', 'es', 'et', 'eu', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gg', 'gh', 'gi', 'gl', 'gm', 'gn', 'gov', 'gp', 'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'im', 'in', 'info', 'int', 'io', 'iq', 'ir', 'is', 'it', 'je', 'jm', 'jo', 'jobs', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'me', 'mg', 'mh', 'mil', 'mk', 'ml', 'mm', 'mn', 'mo', 'mobi', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'museum', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'name', 'nc', 'ne', 'net', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om', 'org', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pm', 'pn', 'post', 'pr', 'pro', 'ps', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'rs', 'ru', 'rw', 'sa', 'sb', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'st', 'su', 'sv', 'sx', 'sy', 'sz', 'tc', 'td', 'tel', 'tf', 'tg', 'th', 'tj', 'tk', 'tl', 'tm', 'tn', 'to', 'tp', 'tr', 'travel', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 'uk', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws','xxx', 'ye', 'yt', 'za', 'zm', 'zw']
+    domainset=set(domainset)
+    lisy=[]
+    for domain in bagofwords:
+      if domain in domainset:
+        lisy.append(domain)
+    return lisy
+    
+  def GetTokenCount(self, bagofwords):
+    count=0
+    for elem in bagofwords:
+      if elem != '':
+        count=count+1
+    return count
+   
+    
+  def GetTotalTokenLength(self, bagofwords):
+    totallength=0
+    for elem in bagofwords:
+      if elem != '':
+        totallength=totallength+len(elem)
+    return totallength
+
+  def GetLongestTokenLength(self, bagofwords):
+    maxlength=0
+    for elem in bagofwords:
+      if len(elem)>maxlength:
+        maxlength=len(elem)
+    return maxlength
 
 if __name__=="__main__":
   #give the file name below
