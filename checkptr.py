61bc52454814248cf509a8d228d86264b280e5d8
Haotian Liu <liuhaotian.air@gmail.com>
2012-11-28 11:24:04 -0600

    fix ptr bug

--- checkptr.py
+++ checkptr.py
@@ -13,12 +13,15 @@ import socket
 def checkPTR(url):
     try:
         urlObj  = urlparse(url)
-        return socket.getfqdn(urlObj.netloc) == urlObj.netloc
+        #return socket.getfqdn(urlObj.netloc) == urlObj.netloc
+        allips  = [x[4][0] for x in socket.getaddrinfo(urlObj.netloc,80) if ':' not in x[4][0]]
+        subips  = [x[4][0] for x in socket.getaddrinfo(socket.gethostbyaddr(urlObj.netloc)[0],80) if ':' not in x[4][0]]
+        return all([x in allips for x in subips])
     except Exception, e:
         return False
 
 def main():
-    print checkPTR('http://twitter.com/cnnbrk')
+    print checkPTR('http://www.renren.com/cnnbrk')
     print '=' * 50
     print checkPTR('http://t.co/xdBHUbHc')
 
